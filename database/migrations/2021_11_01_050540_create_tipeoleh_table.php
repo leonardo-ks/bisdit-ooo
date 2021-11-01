ef67f10bc6201f0751331239f39b2a5915192713
Farhan Septiadi <55498270+suzuya26@users.noreply.github.com>
2021-11-01 12:15:28 +0700

    migration masih wip
    file migration tableny sudah siap, isian blum tapi

---
+++ database/migrations/2021_11_01_050540_create_tipeoleh_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipeolehTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipeoleh', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipeoleh');
+    }
+}
